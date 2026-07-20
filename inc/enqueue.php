5fe896fbee0084a0c9a22a789d216a1da6e34b2c
truewebmaster <workmylife@yandex.ru>
2026-07-20 13:45:48 +0300

    Настроено подключение стилей и скриптов

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -3,10 +3,9 @@
 defined('ABSPATH') || exit;
 
 /**
- * Подключение стилей и скриптов темы
+ * Подключение стилей темы.
  */
-
-function tw_enqueue_assets(): void
+function tw_enqueue_styles(): void
 {
     wp_enqueue_style(
         'tw-main',
@@ -15,4 +14,20 @@ function tw_enqueue_assets(): void
         wp_get_theme()->get('Version')
     );
 }
-add_action('wp_enqueue_scripts', 'tw_enqueue_assets');
+
+/**
+ * Подключение скриптов темы.
+ */
+function tw_enqueue_scripts(): void
+{
+	    wp_enqueue_script(
+        'tw-main',
+        get_template_directory_uri() . '/assets/js/main.js',
+        [],
+        wp_get_theme()->get('Version'),
+        true
+    );
+}
+
+add_action('wp_enqueue_scripts', 'tw_enqueue_styles');
+add_action('wp_enqueue_scripts', 'tw_enqueue_scripts');
