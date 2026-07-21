17ef5ff5c9a57e86bfe3a32ee02e72e881bb9de0
truewebmaster <workmylife@yandex.ru>
2026-07-21 11:58:29 +0300

    v1.3.1 Создан загрузчик init.php

--- functions.php
+++ functions.php
@@ -2,15 +2,5 @@
 
 defined('ABSPATH') || exit;
 
-/**
- * Подключение файлов темы
- */
 
-require_once get_template_directory() . '/inc/constants.php';
-require_once get_template_directory() . '/inc/enqueue.php';
-require_once get_template_directory() . '/inc/theme-support.php';
-require_once get_template_directory() . '/inc/menus.php';
-require_once get_template_directory() . '/inc/helpers.php';
-require_once get_template_directory() . '/inc/theme-options.php';
-
-echo TW_THEME_VERSION;
+require_once get_template_directory() . '/inc/init.php';
---
+++ inc/init.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('ABSPATH') || exit;
+
+
+require_once __DIR__ . '/constants.php';
+require_once __DIR__ . '/theme-support.php';
+require_once __DIR__ . '/menus.php';
+require_once __DIR__ . '/enqueue.php';
+require_once __DIR__ . '/helpers.php';
