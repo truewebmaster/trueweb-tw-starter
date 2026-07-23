8a4f20ceebf133014f6cb9f61eb11bf31d20b453
truewebmaster <workmylife@yandex.ru>
2026-07-23 15:47:58 +0300

    header +

--- template-parts/header/header-main.php
+++ template-parts/header/header-main.php
@@ -1,33 +1,31 @@
 <header class="header" data-js-header>
-      <div class="header__body">
-        <div class="header__body-inner container">
-          <div class="header__logo">
-        		<?php
-        		if (has_custom_logo()) {
-            	the_custom_logo();
-        		} else {
-            bloginfo('name');
-        		}
-        		?>
-    </div>
-          <!-- <a
-            class="header__action-phone header__action-phone--logo"
-            href="tel:+79255638484"
-            >+7 (925) 563-84-84</a
-          > -->
-
-          <div class="header__overlay" data-js-header-overlay>
-            <nav class="header__menu">
-							<?php
-								wp_nav_menu([
-    							'theme_location' => 'primary',
-    							'container' => false,
-    							'menu_class' => 'header__menu-list',
-                  'fallback_cb' => false,
-								]);
-								?>
-						</nav>
-            <div class="header__action">
+      <div class="header__wrapper">
+        <div class="container">
+            <div class="header__inner">
+              <div class="header__logo logo">
+              <?php if (has_custom_logo()) : ?>
+                  <?php the_custom_logo(); ?>
+              <?php else : ?>
+                  <a
+                      href="<?= esc_url(home_url('/')); ?>"
+                      class="logo__text"
+                  >
+                      <?= esc_html(get_bloginfo('name')); ?>
+                  </a>
+              <?php endif; ?>
+              </div>
+              <div class="header__overlay" data-js-header-overlay>
+                <nav class="header__menu">
+					      	<?php
+					      		wp_nav_menu([
+    			      			'theme_location' => 'primary',
+    			      			'container' => false,
+    			      			'menu_class' => 'header__menu-list',
+                      'fallback_cb' => false,
+					      		]);
+					      		?>
+					      </nav>
+                <div class="header__action">
               <div class="header__action-info">
                 <a class="header__action-phone" href="tel:+79999999999"
                   >+7 (999) 999-99-99</a
@@ -43,14 +41,15 @@
               >
                 Заказать звонок
               </button>
-            </div>
-          </div>
+                </div>
+              </div>
 
           <div class="header__mob-action visible-tablet">
            
 					<? get_template_part('template-parts/components/burger-button'); ?>
 
           </div>
+              </div>
         </div>
       </div>
     </header>
